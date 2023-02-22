a6fc7e6fbd6a1253e5ad1c9ace477bece0d72571
Atroopa <atroopa@gmail.com>
2023-02-22 17:48:20 +0330

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# URL of the API endpoint
+API_URL = 'https://api.binance.com'
+
+# Symbols to fetch data for
+SYMBOLS = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT']
+
+# Time frame for the data (options: '1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M')
+TIME_FRAME = '1h'
+
+# Maximum number of data points to fetch
+LIMIT = 1000
